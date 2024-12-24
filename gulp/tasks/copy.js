316b839a632017abd14be847a41fda0214190903
ILGIZ <ilgiz-87@mail.ru>
2024-12-24 11:44:29 +0500

    Сборка от frontend-case

--- gulp/tasks/copy.js
+++
@@ -1,9 +0,0 @@
-export const copy = () => {
-    return app.gulp.src(app.path.src.files)
-    .pipe(app.gulp.dest(app.path.build.files))
-}
-
-export const copyFonts = () => {
-    return app.gulp.src(`${app.path.srcFolder}/fonts/*.woff2`, { encoding: false })
-          .pipe(app.gulp.dest(`${app.path.build.fonts}`));
-}
